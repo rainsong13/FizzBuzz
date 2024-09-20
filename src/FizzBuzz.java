ac6b1020bdcc40da665e69f93f4967468a8d7a17
rainsong13 <1419046851@qq.com>
2024-09-20 12:53:34 +0800

    all task

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -9,11 +9,11 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            i = fizzbuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int fizzbuzz(int i) {
+    static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+
+    FizzBuzz.doFizzBuzz(5);
+
+}
